ba9f48ef5bcb8c0c4930530861d0e21711894586
fp024 <aopenasus@naver.com>
2025-03-01 02:46:00 +0900

    ~ p27: 1.4.2 스프링 부트 애플리케이션 이벤트 감지 - `@EventListener`

--- chap01/chap01-boot3/src/main/java/org/springboot/practice/BootApplication.java
+++ chap01/chap01-boot3/src/main/java/org/springboot/practice/BootApplication.java
@@ -1,12 +1,21 @@
 package org.springboot.practice;
 
+import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class BootApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BootApplication.class, args);
+  }
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void applicationReadyEvent(ApplicationReadyEvent applicationReadyEvent) {
+    System.out.println(
+        "Application Ready Event generated at " + new Date(applicationReadyEvent.getTimestamp()));
   }
 }
