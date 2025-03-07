5f27ac5a44a284ed5089d2d099f3f382be016f80
fp024 <aopenasus@naver.com>
2025-03-07 11:47:18 +0900

    ~ p127: 3.4.6 기법: 관계형 데이터베이스에 저장된 도메인 객체를 NamedQuery로 조회

---
+++ chap03/boot3/named-query-method/src/main/java/org/springboot/practice/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package org.springboot.practice.repository;
+
+import org.springboot.practice.model.Course;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends CrudRepository<Course, Long> {
+
+  Iterable<Course> findAllByCategoryAndRating(String category, int rating);
+}
