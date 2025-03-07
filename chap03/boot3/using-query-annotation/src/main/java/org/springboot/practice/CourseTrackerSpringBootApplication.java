7b65189f75391b574a3842af12d3e24fc1d5d56f
fp024 <aopenasus@naver.com>
2025-03-07 12:02:12 +0900

    ~ p132: 3.5.1 기법 `@Query` 애너테이션을 사용해서 쿼리를 정의하고 관계형 데이터베이스에 저장된 도메인 객체 조회

---
+++ chap03/boot3/using-query-annotation/src/main/java/org/springboot/practice/CourseTrackerSpringBootApplication.java
@@ -0,0 +1,14 @@
+package org.springboot.practice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class CourseTrackerSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CourseTrackerSpringBootApplication.class, args);
+  }
+}
