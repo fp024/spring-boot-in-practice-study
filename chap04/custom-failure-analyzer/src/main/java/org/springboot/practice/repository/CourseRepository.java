63cbb57f53fe293a3fe2e0bce165fbc3e5997ffc
fp024 <aopenasus@naver.com>
2025-03-13 02:24:29 +0900

    ~ p179: 4.4.1 기법: 스프링 부트 액추에이터 설정
    먼저 했던 예제에서 주제와 관계 없어, 의미 없는 DB 설정들은 제거했다.

--- chap04/custom-failure-analyzer/src/main/java/org/springboot/practice/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package org.springboot.practice.repository;
-
-import org.springboot.practice.model.Course;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends CrudRepository<Course, Long> {}
