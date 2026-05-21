4afe7897ea1673fa7d21ed5616b196783fa380e4
fp024 <aopenasus@naver.com>
2026-05-21 12:03:12 +0900

    ~p247: 5.3.1 기법: 스프링 부트 애플리케이션에서 스프링 시큐리티 로그인 페이지 커스터마이징
    오랜만에 예제를 추가해보려니 머리가 띵하다..😂

---
+++ chap05/implementing-custom-loginpage/src/main/java/org/springboot/practice/service/CourseService.java
@@ -0,0 +1,17 @@
+package org.springboot.practice.service;
+
+import java.util.Optional;
+import org.springboot.practice.model.Course;
+
+public interface CourseService {
+
+  Course createCourse(Course course);
+
+  Optional<Course> findCourseById(Long courseId);
+
+  Iterable<Course> findAllCourses();
+
+  Course updateCourse(Course course);
+
+  void deleteCourseById(Long courseId);
+}
