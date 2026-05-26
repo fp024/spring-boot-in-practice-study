1da148cad596e335551341c183ed09152aa7adee
fp024 <aopenasus@naver.com>
2026-05-26 10:05:24 +0900

    p265: 5.3.5 기법: LDAP 인증 적용
    - ROLE도 LDAP로부터 가져오도록 기존 예제 변경
    - 암호는 지금 자동선택되는 암호 인코더 쓰기 위해 ldap 파일의 암호 부분에다 {noop} 붙여줬다.

--- chap05/ldap-authentication/src/main/java/org/springboot/practice/security/SecurityConfiguration.java
+++ chap05/ldap-authentication/src/main/java/org/springboot/practice/security/SecurityConfiguration.java
@@ -1,14 +1,16 @@
 package org.springboot.practice.security;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springboot.practice.repository.ApplicationUserRepository;
-import org.springboot.practice.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.ldap.LdapPasswordComparisonAuthenticationManagerFactory;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -52,7 +54,31 @@ public class SecurityConfiguration {
   }
 
   @Bean
-  UserDetailsService userDetailsService(ApplicationUserRepository applicationUserRepository) {
-    return new CustomUserDetailsService(applicationUserRepository);
+  DefaultSpringSecurityContextSource contextSource() {
+    return new DefaultSpringSecurityContextSource(
+        List.of("ldap://localhost:8389/"), //
+        "dc=manning,dc=com" //
+        );
+  }
+
+  @Bean
+  AuthenticationManager authenticationManager(
+      DefaultSpringSecurityContextSource contextSource, PasswordEncoder passwordEncoder) {
+    // 기존 passwordCompare() 설정과 동일한 방식
+    var factory =
+        new LdapPasswordComparisonAuthenticationManagerFactory(contextSource, passwordEncoder);
+    factory.setUserDnPatterns("uid={0},ou=people");
+    factory.setPasswordAttribute("userPassword");
+
+    // Role 정보를 LDAP에서 가져오도록 설정
+    factory.setLdapAuthoritiesPopulator(
+        new DefaultLdapAuthoritiesPopulator(contextSource, "ou=groups") {
+          {
+            setGroupSearchFilter("member={0}");
+            setGroupRoleAttribute("cn");
+          }
+        });
+
+    return factory.createAuthenticationManager();
   }
 }
--- chap05/ldap-authentication/src/main/resources/data.sql
+++ chap05/ldap-authentication/src/main/resources/data.sql
@@ -6,19 +6,3 @@ VALUES ('Getting Started with Spring Security DSL', 'Spring', 5, 'Learn Spring S
 INSERT INTO COURSES(NAME, CATEGORY, RATING, DESCRIPTION)
 VALUES ('Getting Started with Spring Cloud Kubernetes', 'Spring', 3,
         'Master Spring Boot Application Deployment with Kubernetes');
-
-
-INSERT INTO CT_USERS(ID, FIRST_NAME, LAST_NAME, USERNAME, PASSWORD, EMAIL, VERIFIED, LOCKED, ACC_CRED_EXPIRED)
-VALUES (1, 'John', 'Socket', 'jsocket', '{noop}pw00', 'jsocket@example.com', TRUE, FALSE, FALSE);
-INSERT INTO CT_USERS(ID, FIRST_NAME, LAST_NAME, USERNAME, PASSWORD, EMAIL, VERIFIED, LOCKED, ACC_CRED_EXPIRED)
-VALUES (2, 'Steve', 'Smith', 'smith', '{noop}pw00', 'smith@example.com', FALSE, FALSE, FALSE);
-INSERT INTO CT_USERS(ID, FIRST_NAME, LAST_NAME, USERNAME, PASSWORD, EMAIL, VERIFIED, LOCKED, ACC_CRED_EXPIRED)
-VALUES (3, 'Ken', 'Masters', 'ken', '{noop}pw00', 'ken@example.com', TRUE, FALSE, FALSE);
-
-
-INSERT INTO CT_AUTHORITIES(USERNAME, AUTHORITY)
-VALUES ('jsocket', 'ROLE_ADMIN');
-INSERT INTO CT_AUTHORITIES(USERNAME, AUTHORITY)
-VALUES ('smith', 'ROLE_USER');
-INSERT INTO CT_AUTHORITIES(USERNAME, AUTHORITY)
-VALUES ('ken', 'ROLE_USER');
